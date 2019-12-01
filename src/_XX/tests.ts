6d2dd270853880015fd23c57fb9434a66a960a0d
enapah <peter.kedemo@gmail.com>
2019-12-01 12:08:10 +0100

    template for new solutions

---
+++ src/_XX/tests.ts
@@ -0,0 +1,17 @@
+import {assert} from 'chai';
+
+import {part1, part2} from './solution';
+
+describe('Day XX', () => {
+  describe('part 1', () => {
+    it('examples', () => {});
+
+    it('solution', () => {});
+  });
+
+  describe('part 2', () => {
+    it('examples', () => {});
+
+    it('solution', () => {});
+  });
+});
---
+++ src/utils/read-input.ts
@@ -0,0 +1,4 @@
+import fs from 'fs';
+
+export const readInput = (dirname: string) =>
+  fs.readFileSync(`${dirname}/input`, 'utf8').split('\n');
